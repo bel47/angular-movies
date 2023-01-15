4b47001e6237410bf2ff64e81221eb25bd85b2fc
Belay Retta <tony.retta@gmail.com>
2023-01-15 10:48:44 +0300

    modfied app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-movies';
+
+  movies = [{
+    title: 'Spider man',
+    releaseDate: new Date(),
+    price: 1400.99
+  },
+  {
+    title: 'Moana',
+    releaseDate: new Date("2016-16-11"),
+    price: 450.99
+  },
+  {
+    title: 'Spider man',
+    releaseDate: new Date(),
+    price: 300.99
+  }];
+
+  dupulictaeNumber(n:any){
+    return n*2;
+  }
 }
